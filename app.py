a380caf9b23dadcde4ac85bc304d1ea7536be78d
syedkumailhaiderzaidi69-coder <syedkumailhaiderzaidi69@gmail.com>
2026-05-30 02:18:26 +0400

    Update app.py

--- app.py
+++ app.py
@@ -154,27 +154,10 @@ except:
 # ---------- INVESTMENT SCORING MODEL ----------
 
 max_price = df["Price"].max()
-
-df["Affordability Score"] = (
-    100 - (df["Average Price"] / max_price * 100)
-)
-
-df["Market Confidence"] = np.random.randint(
-    75,
-    96,
-    size=len(df)
-)
-
-df["Luxury Demand"] = np.random.randint(
-    70,
-    98,
-    size=len(df)
-)
-df["Rental Yield"] = np.random.uniform(
-    5.0,
-    9.5,
-    size=len(df)
-).round(1)
+df["Affordability Score"] = 100 - (df["Price"] / max_price * 100)
+df["Market Confidence"] = np.random.randint(75, 96, size=len(df))
+df["Luxury Demand"] = np.random.randint(70, 98, size=len(df))
+df["Rental Yield"] = np.random.uniform(5.0, 9.5, size=len(df)).round(1)
 
 df["Investment Score"] = (
     df["Projected Growth"] * 4 * 0.35 +
