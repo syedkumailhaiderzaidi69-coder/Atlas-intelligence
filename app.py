a492021a36da7b09b30506ae00032bd93adc21d3
syedkumailhaiderzaidi69-coder <syedkumailhaiderzaidi69@gmail.com>
2026-05-30 23:11:49 +0400

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 import duckdb
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
 import time
 from datetime import datetime
 from io import BytesIO
