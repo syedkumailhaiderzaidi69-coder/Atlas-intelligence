dc3ac6ca786d18d550c210452dfc30787ef453c8
syedkumailhaiderzaidi69-coder <syedkumailhaiderzaidi69@gmail.com>
2026-05-31 15:02:27 +0400

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 import duckdb
+from sqlalchemy import create_engine
+import os
 from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
