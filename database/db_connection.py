2c0c88d350d99bbabc2127e9c1cb7bdf964051dc
syedkumailhaiderzaidi69-coder <syedkumailhaiderzaidi69@gmail.com>
2026-06-05 18:03:07 +0400

    Update db_connection.py

--- database/db_connection.py
+++ database/db_connection.py
@@ -1,16 +1,15 @@
-import streamlit as st
-
 from sqlalchemy import create_engine
-
-
-@st.cache_resource
+import pandas as pd
 
 def get_database_connection():
-
-    DATABASE_URL = st.secrets["DATABASE_URL"]
-
-    engine = create_engine(
-        DATABASE_URL
-    )
-
+    # Replace with your Supabase connection URL
+    DATABASE_URL = "postgresql://postgresql://postgres.wzfyczdilwsiicaihcgh:Nvidia67890---@aws-1-ap-southeast-1.pooler.supabase.com:5432/postgres"
+    
+    engine = create_engine(DATABASE_URL)
     return engine
+
+def load_full_transactions():
+    engine = get_database_connection()
+    query = "SELECT * FROM dubai_transactions_master"
+    df = pd.read_sql(query, engine)
+    return df
